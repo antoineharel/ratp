47bb7a3720ae9e7b9446517f3ffaf594ea170e1c
Antoine Harel <dev@antoineharel.fr>
2022-02-20 22:53:48 +0100

    Added title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLoaderData } from "remix";
+import { useLoaderData } from "remix";
 import TinyTrafficCard from "~/components/TinyTrafficCard";
 import TinyTrafficGrid from "~/components/TinyTrafficGrid";
 import { ratp } from "~/lib/ratp";
@@ -14,8 +14,13 @@ export default function Index() {
   const { metros, rers, tramways } = useLoaderData<Data>();
 
   return (
-    <>
+    <div className="flex items-center justify-center h-screen m-auto">
       <div className="sm:w-[640px] p-10 mx-auto space-y-8 md:space-y-10">
+        <h1 className="flex items-center gap-4 text-2xl font-bold text-center text-gray-600 sm:text-3xl">
+          <div className="w-full h-px bg-gray-300"></div>
+          <span className="flex-shrink-0">État du trafic</span>
+          <div className="w-full h-px bg-gray-300"></div>
+        </h1>
         <div className="space-y-3 sm:space-y-4">
           <h2 className="flex items-center gap-1 sm:gap-1.5 text-xl font-semibold sm:text-xl md:text-2xl">
             <img className="w-6 sm:w-7 md:w-8" src="/img/metro.svg" />
@@ -50,6 +55,6 @@ export default function Index() {
           </TinyTrafficGrid>
         </div>
       </div>
-    </>
+    </div>
   );
 }
